81a29cd81f644114b187da3c6671d10b48bfe7f4
Israa El-halabi <israa00elhalby@gmail.com>
2025-09-12 21:25:39 +0300

    Update note_model.dart

--- lib/data/note_model.dart
+++ lib/data/note_model.dart
@@ -2,13 +2,15 @@
   final String id;
   final String title;
   final String content;
- 
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   NoteModel({
     required this.id,
     required this.title,
     required this.content,
- 
+    required this.createdAt,
+    required this.updatedAt,
   });
 
   factory NoteModel.fromJson(Map<String, dynamic> json) {
@@ -16,6 +18,8 @@
       id: json['id'],
       title: json['title'],
       content: json['content'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt']),
     );
   }
 
@@ -24,6 +28,8 @@
       'id': id,
       'title': title,
       'content': content,
+      'createdAt': createdAt.toIso8601String(),
+      'updatedAt': updatedAt.toIso8601String(),
     };
   }
  }
